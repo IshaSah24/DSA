d4687eb32e82001522c323141eb483189e98c559
Isha Sah <131801898+IshaSah24@users.noreply.github.com>
2025-07-12 01:08:59 +0530

    Added dsa content about deletion and insertion using LL with separate functions

--- 07-LinkedList/deletion.cpp
+++ 07-LinkedList/deletion.cpp
@@ -58,17 +58,60 @@ Node* removeNode (Node* head, int position){
 	return head;
 }
 
+Node * removeVal(Node* head, int  val){
+	Node* prev = head;
+	if(head == NULL) return NULL;
+	if(head->data == val){
+		Node* temp = head;
+		head = head->next;
+		delete(temp);
+	}
+
+	while (prev->next->data != val  && prev->next != NULL){
+		prev = prev->next;
+	}
+	if (prev->next != NULL){
+		Node* tempToDel = prev->next;
+		prev->next = prev->next->next;
+
+		delete(tempToDel);
+	}
+
+
+
+	return head;
+}
+
+
+Node* insertNode (Node* head, int  val){
+	Node* temp = new Node(val);
+	temp->next = head;
+	head = temp;
+	return head;
+}
+
 int main (){
 	int arr [] = {12,34,56,78,98};
 	int  size = sizeof(arr) / sizeof(arr[0]);
 
 	Node* headptr = createLL(arr, size);
+	cout << "Initial Node : ";
 	print(headptr);
 
     // new headptr after deleting head
+    cout << "Removing Node : ";
 	headptr = removeNode(headptr, 4); 
+	print(headptr);
+
+	cout << "Removing Value : " ;
+	headptr = removeVal(headptr, 98);
+	print(headptr);
+
+	cout << "Inserting Node : ";
+	headptr = insertNode(headptr, 100);
 
 	// cout << headptr->data << endl;
+	
 	print(headptr);
 
 }
